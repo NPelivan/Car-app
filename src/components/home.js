479001f6731b3be09e06e9ca38c2b115e8a884e2
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2021-04-08 10:40:20 +0200

    mobx problem

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 
-import "../layouts/Home styles/style.css";
-import { data } from "./cars";
+import { inject, observer } from "mobx-react";
 
+@inject("CarStore")
+@observer
 export default class Home extends React.Component {
+	sort = (e) => {
+		this.props.CarStore.isSorted = true;
+	};
 	render() {
+		const { filter } = this.props.CarStore;
 		return (
 			<main>
 				<h1>All Cars</h1>
 
-				{data.map(function (item) {
-					return (
-						<div>
-							<h2>{item.carname}</h2>
+				{this.props.CarStore.filter((car) => car !== null).map((car) => (
+					<div>
+						<h2>{car.carname}</h2>
 
-							<span>Model: {item.model}</span>
+						<button onClick={this.sortx}>Sort A-Z</button>
 
-							<span>Mileage: {item.mileage}</span>
+						<span>Model: {car.model}</span>
 
-							<span>Year: {item.year}</span>
-						</div>
-					);
-				})}
+						<span>Mileage: {car.mileage}</span>
+
+						<span>Year: {car.year}</span>
+					</div>
+				))}
 			</main>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import CarStore from "./components/cars";
+import { Provider } from "mobx-react";
 
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider CarStore={CarStore}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById("root")
 );
 
